2c44bdf8541e343583745dd24da15a7b251f3e48
Alistair Stead <alistair.stead@designdisclosure.com>
2010-01-16 10:14:04 +0000

    removed the apc task and moved it into the PHP namespace

--- lib/capistrano/ext/ubuntu-machine/apc.rb
+++
@@ -1,8 +0,0 @@
-namespace :apc do
-  desc "Install APC opcode cache using ubuntu package"
-  task :install, :roles => :app do    
-    sudo "apt-get install php-apc -y"
-    sudo "/etc/init.d/apache2 reload"
-  end
-
-end
